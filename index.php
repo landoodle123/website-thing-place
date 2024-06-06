e06c8463634217ce3febd14f40a1b451eea1e5bf
landoodle123 <landonkazen@gmail.com>
2024-06-06 17:50:08 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Website Thing Place</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="topnav">
+      <a class="active" href="index.php">Home</a>
+      <a href="feedback.php">Send Feedback</a>
+      <a href="time.php">View current time</a>
+    </div>
+    <h1>Welcome to Website Thing Place</h1>
+    <p>Website Thing Place is a website-like thing place of yesness.</p>
+</html>

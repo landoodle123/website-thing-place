47125f191f069e04b2baf0f59d3d4c7b28586217
landoodle123 <landonkazen@gmail.com>
2024-06-07 15:37:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
       <a class="active" href="index.php">Home</a>
       <a href="feedback.php">Send Feedback</a>
       <a href="time.php">View current time</a>
+      <a href="about.php">About Us</a>
     </div>
     <h1>Welcome to Website Thing Place</h1>
     <p>Website Thing Place is a website-like thing place of yesness.</p>

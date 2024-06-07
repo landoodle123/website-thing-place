08d37482bafb50c6c378df45264dc29cdecd6bfc
landoodle123 <landonkazen@gmail.com>
2024-06-07 15:38:48 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>About Website Thing Place</title>
+    <link rel="stylesheet" href = "style.css">
+  </head>
+  <body>
+    <div class="topnav">
+      <a href="index.php">Home</a>
+      <a href="feedback.php">Send Feedback</a>
+      <a href="time.php">View current time</a>
+      <a class="active" href="about.php">About Us</a>
+    </div>
+    <h1>About Us</h1>
+    <p>Website Thing Place is a website-like thing place of yesness.</p>
+</html>
